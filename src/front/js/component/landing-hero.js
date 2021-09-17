2e6b9a039b0d739e4cb1aa066a6031988cd69757
LeonErasmusCh <leondevcode@gmail.com>
2021-09-17 00:24:52 +0000

    Landing

---
+++ src/front/js/component/landing-hero.js
@@ -0,0 +1,25 @@
+import React from "react";
+import heroImg from "../../img/heroImg.png";
+
+export const HeroSection = () => {
+	return (
+		<div>
+			<div id="heroSection" className="container-fluid">
+				<div className="row pt-5 h-100">
+					<div className="col-11 col-sm-6">
+						<h1 id="heroH1" className="text-white p-5">
+							Hola! Somos Fleet y aqui es donde juntamos vendedores y repartidores
+						</h1>
+						<div>
+							<button className="button pl-4">iniciar seccion</button>
+							<button className="button pl-4">subscribe</button>
+						</div>
+					</div>
+					<div className="col-11 col-sm-6 p-5">
+						<img className="heroImg" src={heroImg} alt="Despachos" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,14 +3,45 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg fixed-top">
+			<div className="container-fluid">
+				<a className="navbar-brand text-white" href="#">
+					Fleet Logo
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">
+								inicio
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								preguntas frecuentes
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								quienes somos
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								contactos
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
